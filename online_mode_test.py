7460bf1fb82970f5f91674d416e36426552baf00
alauters <austin-lauters@uiowa.edu>
2019-04-26 11:26:02 -0500

    bare bones test cause im struggling

--- online_mode_test.py
+++ online_mode_test.py
@@ -19,47 +19,8 @@ while (i != 60):
     mode = db.child("mode").get().val()
     voltage = db.child("voltage_control").get().val()
 
-    #########################
-    # work done based on mode (5 GPIO)
-    #
-    # Current setup Little sphere is connected positive and negative
-    # 1 & 7, 12 & 5, 11 & 6 are dead (off) postion
-    # 2 & 8  = Top Needle, Big Sphere           Aurora
-    # 3 & 9  = Second Needle, Little sphere-    Belts (auroral lobe)
-    # 4 & 10 = Little sphere+, Big Sphere       Ring Current
-    #########################
-    if (mode == "Aurora"):
-        db.child("ui-planeterrella").child("voltage").update("1")
-        print ("%s" %mode)
+    db.child("ui-planeterrella").update("voltage":"50")
 
-    elif (mode == "Belt"):
-        db.child("ui-planeterrella").child("voltage").update("50")
-        print ("%s" %mode)
-
-    elif (mode == "Ring"):
-        db.child("ui-planeterrella").child("voltage").update("99")
-        print ("%s" %mode)
-
-    else:
-        print ("Invalid mode type passed: %s " %(mode)) 
-
-    #################################
-    # Work done based on air_pressure
-    #################################
-    if (air_pressure == "Low"):
-        db.child("ui-planeterrella").child("current").update("1")
-        print ("%s\n" %air_pressure)
-
-    elif (air_pressure == "Medium"):
-        db.child("ui-planeterrella").child("current").update("50")
-        print ("%s\n" %air_pressure)
-
-    elif (air_pressure == "High"):
-        db.child("ui-planeterrella").child("current").update("99")
-        print ("%s\n" %air_pressure)
-        
-    else:
-        print ("Invalid air_pressure type passed: %s" %(air_pressure))
 
 
 
